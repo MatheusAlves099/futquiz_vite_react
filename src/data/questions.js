3580240f09898e65d42a3c81148ff55368df850a
MatheusAlves099 <mts02501@gmail.com>
2023-07-25 09:02:25 -0300

    Projeto finalizado

---
+++ src/data/questions.js
@@ -0,0 +1,61 @@
+const data = [
+  {
+    question: "Qual time é o atual campeão da Copa do Brasil?",
+    options: ["Flamengo", "São Paulo", "Palmeiras", "Athletico Paranaense",
+    ],
+    answer: "Flamengo",
+  },
+  {
+    question: "Qual seleção conquistou a Copa do Mundo do QATAR de 2022?",
+    options: ["França", "Argentina", "Brasil", "Croácia"],
+    answer: "Argentina",
+  },
+  {
+    question: "Qual o clube atual do jogador Cristiano Ronaldo?",
+    options: ["Manchester United", "Sporting", "Al-Nassr", "PSG"],
+    answer: "Al-Nassr",
+  },
+  {
+    question: "Quantas vezes a seleção brasileira conquistou a Copa do Mundo?",
+    options: ["2", "4", "6", "5"],
+    answer: "5",
+  },
+  {
+    question:
+      "Qual é o clube de futebol mais antigo do mundo?",
+    options: ["Sheffield FC", "Crystal Palace", "Notts County", "Rio Grande"],
+    answer: "Sheffield FC",
+  },
+  {
+    question:
+      "Quantas vezes a jogadora Marta foi eleita como a melhor do mundo?",
+    options: ["3", "5", "8", "6"],
+    answer: "6",
+  },
+  {
+    question:
+      "Quantos jogos Rogério Ceni fez pelo São Paulo FC?",
+    options: ["1237", "1108", "956", "890"],
+    answer: "1237",
+  },
+  {
+    question:
+      "Quem é o maior artilheiro em atividade no mundo atualmente?",
+    options: ["Cristiano Ronaldo", "Lionel Messi", "Luis Suárez", "Robert Lewandowski"],
+    answer: "Cristiano Ronaldo",
+  },
+  {
+    question:
+      "Em qual estádio Pelé marcou seu milésimo gol?",
+    options: ["Maracanã", "Morumbi", "Beira-Rio", "Mineirão"],
+    answer: "Maracanã",
+  },
+  {
+    question:
+      "Qual time brasileiro tem mais títulos mundiais?",
+    options: ["São Paulo", "Flamengo", "Santos", "Palmeiras"],
+    answer: "São Paulo",
+  },
+];
+
+export default data;
